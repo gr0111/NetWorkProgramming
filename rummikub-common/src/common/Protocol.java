1b437a5a5d3fd3648ebbf8cf3d7e54992c29a326
kimgaram <gr_0111@naver.com>
2025-11-24 00:45:16 +0900

    공통 부분 내용 추가

--- rummikub-common/src/common/Protocol.java
+++ rummikub-common/src/common/Protocol.java
@@ -18,9 +18,17 @@ public class Protocol {
     public static final String CHAT = "CHAT";      // 일반 채팅 메시지
     public static final String INFO = "INFO";      // 서버 정보/알림
 
-     // --- 새로 추가된 프로토콜 신호 ---
+    // --- 새로 추가된 프로토콜 신호 ---
     public static final String OWNER         = "OWNER";         // 방장 알림 (예: OWNER|true)
     public static final String PLAYER_COUNT  = "PLAYER_COUNT"; // 현재 인원수 알림 (예: PLAYER_COUNT|3)
     
+    public static final String GAME_START = "GAME_START";   // GAME_START|인원수
+    public static final String INITIAL_TILES = "INITIAL_TILES";  // INITIAL_TILES|R1,BL3,Y10,...
+    public static final String WIN = "WIN";  // WIN|플레이어이름
+    public static final String GAME_OVER = "GAME_OVER"; // GAME_OVER|플레이어이름
+    public static final String BOARD = "BOARD";  // BOARD|R1,R2,R3;BL5,BL6,BL7
+    public static final String PLAY_OK = "PLAY_OK";  // PLAY_OK|플레이어이름|보드상태
+    public static final String PLAY_FAIL = "PLAY_FAIL";  // PLAY_FAIL
+
     private Protocol() {} // 인스턴스 생성 방지
 }
